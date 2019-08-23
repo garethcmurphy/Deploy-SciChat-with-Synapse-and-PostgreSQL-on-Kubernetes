f6b0185fbac3e8831caa8384484d2857b0398580
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-23 13:37:28 +0200

    add postgres

--- deployscichat.sh
+++ deployscichat.sh
@@ -2,12 +2,14 @@
 git pull
 helm del --purge scichat
 helm del --purge synapse
+helm del --purge local-postgresql
 
 if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     INGRESS_NAME="-f ./scichat/lund.yaml"
-elif [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
+    elif [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
     INGRESS_NAME="-f ./scichat/dmscprod.yaml"
 fi
 
+helm install stable/postgresql  --namespace $LOCAL_ENV --name local-postgresql
 helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
 helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
