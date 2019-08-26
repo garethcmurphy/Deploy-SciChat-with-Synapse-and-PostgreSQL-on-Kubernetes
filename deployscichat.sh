db74feb434a34763dc8083ad8316fb0279d1b881
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-26 10:44:14 +0200

    reorder command

--- deployscichat.sh
+++ deployscichat.sh
@@ -10,6 +10,6 @@ if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     INGRESS_NAME="-f ./scichat/dmscprod.yaml"
 fi
 
-helm install stable/postgresql  --namespace dev --name local-postgresql --set postgresqlPassword=${POSTGRES_PASSWORD},postgresqlDatabase=synapse
+helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${POSTGRES_PASSWORD},postgresqlDatabase=synapse stable/postgresql
 #helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
 helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
