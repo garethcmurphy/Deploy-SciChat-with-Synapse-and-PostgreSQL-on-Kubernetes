d82c2395f790d2a125be434a0c73a3a97101f8f5
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-26 11:03:00 +0200

    add postgres

--- deployscichat.sh
+++ deployscichat.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [[ -z "${POSTGRES_PASSWORD}" ]]; then
+  MY_POSTGRES="jvkldfgvnjfgskvgsfnj"
+else
+  MY_POSTGRES="${POSTGRES_PASSWORD}"
+fi
 git pull
 #helm del --purge scichat
 helm del --purge synapse
@@ -10,6 +15,6 @@ if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     INGRESS_NAME="-f ./scichat/dmscprod.yaml"
 fi
 
-helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${POSTGRES_PASSWORD},postgresqlDatabase=synapse,postgresqlUser=synapse stable/postgresql
+helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${MY_POSTGRES},postgresqlDatabase=synapse,postgresqlUser=synapse stable/postgresql
 #helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
 helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
