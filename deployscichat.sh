662e76005495021643fc88f0cdec6f16a84932a2
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-26 11:44:32 +0200

    add deploy

--- deployscichat.sh
+++ deployscichat.sh
@@ -8,7 +8,7 @@ echo "mypostgres" ${MY_POSTGRES}
 git pull
 #helm del --purge scichat
 helm del --purge synapse
-helm del --purge local-postgresql
+#helm del --purge local-postgresql
 
 if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     INGRESS_NAME="-f ./scichat/lund.yaml"
@@ -16,6 +16,6 @@ if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     INGRESS_NAME="-f ./scichat/dmscprod.yaml"
 fi
 
-helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${MY_POSTGRES},postgresqlDatabase=synapse,postgresqlUser=synapse stable/postgresql
+#helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${MY_POSTGRES},postgresqlDatabase=synapse stable/postgresql
 #helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
 helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
