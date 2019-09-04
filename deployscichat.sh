96bac2c554ee65c383c6729a0e9654c7205157ca
Gareth Murphy <garethcmurphy@gmail.com>
2019-09-04 09:44:48 +0200

    add new file

--- deployscichat.sh
+++ deployscichat.sh
@@ -6,8 +6,8 @@ else
 fi
 echo "mypostgres" ${MY_POSTGRES}
 git pull
-#helm del --purge scichat
-helm del --purge synapse
+helm del --purge scichat
+#helm del --purge synapse
 #helm del --purge local-postgresql
 
 if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
@@ -17,5 +17,5 @@ if [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
 fi
 
 #helm install  --namespace dev --name local-postgresql --set postgresqlPassword=${MY_POSTGRES},postgresqlDatabase=synapse stable/postgresql
-#helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
-helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
+helm install scichat --name scichat --namespace dev ${INGRESS_NAME}
+#helm install synapse --name synapse --namespace dev ${INGRESS_NAME}
